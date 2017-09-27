0358cbd51dd026ddd93150e066409d5c001469b0
Natalia Ilina <metlinova86@gmail.com>
2017-09-27 12:18:42 +0300

    add open modal in JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var mainNav = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".header__nav-toggle");
+var buyBtns = document.querySelectorAll(".btn--buy");
+var overlay = document.querySelector(".overlay");
+var modal = document.querySelector(".modal");
 //  скрываем меню по умолчанию
 mainNav.classList.add("main-nav--close");
 
@@ -14,3 +17,20 @@ navToggle.addEventListener("click", function(event) {
       navToggle.classList.remove("header__nav-toggle--close");
     }
 })
+
+for(var i = 0; i < buyBtns.length; i++){
+  buyBtns[i].addEventListener("click", function(event){
+    event.preventDefault();
+    overlay.classList.add("overlay--show");
+    modal.classList.add("modal--show");
+    var modalSubmit = modal.querySelector(".modal__btn");
+    modalSubmit.addEventListener("submit", function() {
+      overlay.classList.remove("overlay--show");
+      modal.classList.remove("modal--show");
+    })
+    overlay.addEventListener("click", function(){
+      overlay.classList.remove("overlay--show");
+      modal.classList.remove("modal--show");
+    })
+  })
+}
