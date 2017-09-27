73e9207d114f4aee08a71ec0c6963f5f8301561e
Natalia Ilina <metlinova86@gmail.com>
2017-09-28 01:27:44 +0300

    исправлены ошибки в JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 var mainNav = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".header__nav-toggle");
-var buyBtns = document.querySelectorAll(".btn--buy");
+var buyBtns = document.querySelectorAll(".buy-btn");
 var overlay = document.querySelector(".overlay");
 var modal = document.querySelector(".modal");
-//  скрываем меню по умолчанию
-mainNav.classList.add("main-nav--close");
+if(modal){
+  var modalSubmit = modal.querySelector(".modal__btn");
+}
 
-navToggle.addEventListener("click", function(event) {
+//  скрываем меню по умолчанию и показываем скрытую кнопку
+mainNav.classList.add("main-nav--close");
+navToggle.classList.remove("header__nav-toggle--nojs");
+
+navToggle.addEventListener("click", function(event){
   event.preventDefault();
-    if(!navToggle.classList.contains("header__nav-toggle--close") &&  mainNav.classList.contains("main-nav--close")){
-      mainNav.classList.remove("main-nav--close");
-      navToggle.classList.add("header__nav-toggle--close");
-    }
-    else {
-      mainNav.classList.add("main-nav--close");
-      navToggle.classList.remove("header__nav-toggle--close");
-    }
+  if(!navToggle.classList.contains("header__nav-toggle--close") &&  mainNav.classList.contains("main-nav--close")){
+    mainNav.classList.remove("main-nav--close");
+    navToggle.classList.add("header__nav-toggle--close");
+  }
+  else {
+    mainNav.classList.add("main-nav--close");
+    navToggle.classList.remove("header__nav-toggle--close");
+  }
 })
 
 for(var i = 0; i < buyBtns.length; i++){
@@ -23,14 +28,15 @@ for(var i = 0; i < buyBtns.length; i++){
     event.preventDefault();
     overlay.classList.add("overlay--show");
     modal.classList.add("modal--show");
-    var modalSubmit = modal.querySelector(".modal__btn");
-    modalSubmit.addEventListener("submit", function() {
-      overlay.classList.remove("overlay--show");
-      modal.classList.remove("modal--show");
-    })
-    overlay.addEventListener("click", function(){
-      overlay.classList.remove("overlay--show");
-      modal.classList.remove("modal--show");
-    })
   })
 }
+
+modalSubmit.addEventListener("submit", function(){
+  overlay.classList.remove("overlay--show");
+  modal.classList.remove("modal--show");
+})
+
+overlay.addEventListener("click", function(){
+  overlay.classList.remove("overlay--show");
+  modal.classList.remove("modal--show");
+})
