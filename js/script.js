1f43b7b88ac0fa4072d6c845238fd948c9f05d38
Natalia Ilina <metlinova86@gmail.com>
2017-09-29 15:47:45 +0300

    добавлены проверки элементов в JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var mainNav = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".header__nav-toggle");
+var navToggle = document.querySelector(".main-nav__toggle");
 var buyBtns = document.querySelectorAll(".buy-btn");
 var overlay = document.querySelector(".overlay");
 var modal = document.querySelector(".modal");
@@ -8,39 +8,44 @@ if(modal){
 }
 
 //  скрываем меню по умолчанию и показываем скрытую кнопку
-mainNav.classList.add("main-nav--close");
-navToggle.classList.remove("header__nav-toggle--nojs");
+if(navToggle){
+  mainNav.classList.add("main-nav--close");
+  navToggle.classList.remove("main-nav__toggle--nojs");
 
-navToggle.addEventListener("click", function(event){
-  event.preventDefault();
-  if(!navToggle.classList.contains("header__nav-toggle--close") &&  mainNav.classList.contains("main-nav--close")){
-    mainNav.classList.remove("main-nav--close");
-    navToggle.classList.add("header__nav-toggle--close");
-  }
-  else {
-    mainNav.classList.add("main-nav--close");
-    navToggle.classList.remove("header__nav-toggle--close");
-  }
-})
-
-for(var i = 0; i < buyBtns.length; i++){
-  buyBtns[i].addEventListener("click", function(event){
+  navToggle.addEventListener("click", function(event){
     event.preventDefault();
-    overlay.classList.add("overlay--show");
-    modal.classList.add("modal--show");
+    if(!navToggle.classList.contains("main-nav__toggle--close") &&  mainNav.classList.contains("main-nav--close")){
+      mainNav.classList.remove("main-nav--close");
+      navToggle.classList.add("main-nav__toggle--close");
+    }
+    else {
+      mainNav.classList.add("main-nav--close");
+      navToggle.classList.remove("main-nav__toggle--close");
+    }
   })
 }
 
-overlay.addEventListener("click", function(){
-  overlay.classList.remove("overlay--show");
-  modal.classList.remove("modal--show");
-})
+if(modal) {
+  for(var i = 0; i < buyBtns.length; i++){
+    buyBtns[i].addEventListener("click", function(event){
+      event.preventDefault();
+      overlay.classList.add("overlay--show");
+      modal.classList.add("modal--show");
+    })
+  }
 
-modal.addEventListener("click", function(event) {
-  event.stopPropagation();
-})
+  overlay.addEventListener("click", function(){
+    overlay.classList.remove("overlay--show");
+    modal.classList.remove("modal--show");
+  })
 
-modalSubmit.addEventListener("submit", function(event){
-  overlay.classList.remove("overlay--show");
-  modal.classList.remove("modal--show");
-})
+  modal.addEventListener("click", function(event) {
+    event.stopPropagation();
+  })
+
+  modalSubmit.addEventListener("submit", function(event){
+    overlay.classList.remove("overlay--show");
+    modal.classList.remove("modal--show");
+  })
+
+}
